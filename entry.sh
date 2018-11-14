4167789e28d43fd7e0f5c6254da8b87b29f11017
sunilkr <badboy16s@gmail.com>
2018-11-14 11:57:50 +0530

    Sanity checks in entry.sh

--- entry.sh
+++ entry.sh
@@ -3,6 +3,32 @@
 MODE=$1
 shift
 
+if which readelf >>/dev/null; then
+    echo "[!] Requirement - readelf: OK"
+else
+    echo "[x] Requirement - readelf: FAIL. Install 'binutils/elfutils' package."
+    exit 128
+fi
+
+if [ $1 = "" ]; then
+    echo "[x] Missing required argument"
+    exit 127
+fi
+
+if [ -f $1 ]; then
+    echo "[!] Target : OK"
+else
+    echo "[x] Target : FAIL ($1 does not exists.)"
+    exit 126
+fi
+
+if readelf -h "$1" 2>/dev/null | grep -q 'Class:[[:space:]]*ELF*'; then
+    echo "[!] Target is ELF: OK"
+else
+    echo "[x] Target is ELF: FAIL"
+    exit 125
+fi
+
 if [ "$MODE" = "debug" ]; then
     ./test-debug.sh "$@"
 elif [ "$MODE" = "release" ]; then
--- test-debug.sh
+++ test-debug.sh
@@ -1,40 +1,39 @@
 #!/usr/bin/env bash
 
-echo "Running tests on: $1"
+echo "[!] Target : $1"
+echo "[!] Test group :DEBUG"
 FAIL_COUNT=0
 
 # stack-protector add function call to '__stack_chk_fail'. Test if this name exist.
 if readelf -s "$1" 2>/dev/null | grep -q '__stack_chk_fail'; then
-    echo "(+) Stack Canary: Yes"
+    echo "[+] Stack Canary: Yes"
 else
     FAIL_COUNT=$((FAIL_COUNT + 1))
-    echo '(-) Stack Canary: No'
+    echo '[-] Stack Canary: No'
 fi
 
 if readelf -W -l "$1" 2>/dev/null | grep -q 'GNU_STACK'; then
     if readelf -W -l "$1" 2>/dev/null | grep 'GNU_STACK' | grep -q 'RWE'; then
-        echo "(-) NX Supported: No"
+        echo "[-] NX Supported: No"
         FAIL_COUNT=$((FAIL_COUNT + 1))
     else
-      echo "(+) NX Supported: Yes"
+        echo "[+] NX Supported: Yes"
     fi
 else
-    echo "(-) NX Supported: No"
+    echo "[-] NX Supported: No"
     FAIL_COUNT=$((FAIL_COUNT + 1))
 fi
 
   # check for PIE support
 if readelf -h "$1" 2>/dev/null | grep -q 'Type:[[:space:]]*EXEC'; then
-    echo '(-) PIE Enabled: No'
+    echo '[-] PIE Enabled: No'
     FAIL_COUNT=$((FAIL_COUNT + 1))
 elif readelf -h "$1" 2>/dev/null | grep -q 'Type:[[:space:]]*DYN'; then
     if readelf -d "$1" 2>/dev/null | grep -q 'DEBUG'; then
-        echo "(+) PIE Enabled: Yes"
+        echo "[+] PIE Enabled: Yes"
     else
-        echo "(+) PIE Enabled: DSO"
+        echo "[+] PIE Enabled: DSO"
     fi
-else
-    echo "(*) PIE Enabled: Not-ELF"
 fi
 
 exit $FAIL_COUNT
